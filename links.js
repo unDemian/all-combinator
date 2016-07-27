0b635c6640ad6ccee0a9cacf8bd05119669693c6
Andrei Demian <undemian@gmail.com>
2016-07-27 21:05:51 +0300

    Add localstorage to mark visited links

--- links.js
+++ links.js
@@ -10,18 +10,60 @@ var Links = (function () {
   var buttonHtml = '<a href="#" id="' + selectorButton + '">Open All</a> | ';
 
   /**
-  * Button click listener
+  * Extension storage wrapper
   */
-  var _openLinks = function() {
-    $.each($(selectorStory), function( key, link ) {
-      window.open(link.href, '_blank');
-    });
+  var storage = {
+    items: {},
+    limit: 300,
+    key: 'savedLinks',
+
+    _initSet: function(items) {
+      if(Object.keys(items).length === 0) {
+        items[this.key] = [];
+        this.items = items;
+        chrome.storage.local.set(this.items);
+      } else {
+        this.items = items;
+      }
+    },
+
+    limitSet: function() {
+      var itemsCount = this.items[this.key].length;
+
+      if(itemsCount > this.limit) {
+        this.items[this.key] = this.items[this.key].slice(itemsCount - this.limit, this.limit);
+        chrome.storage.local.set(this.items);
+      }
+    }
+  };
+
+  var _openLink = function(link) {
+    window.open(link, '_blank');
   };
 
   /**
-  * Initialize links module
+  * Button click listener
   */
-  var init = function() {
+  var _buttonHandler = function() {
+    var i = 0;
+    var initialCount = storage.items[storage.key].length;
+
+    $.each($(selectorStory), function( key, link ) {
+      if(storage.items[storage.key].indexOf(link.href) === -1) {
+        storage.items[storage.key].push(link.href);
+        _openLink(link.href);
+      }
+    });
+
+    if(storage.items[storage.key].length > initialCount) {
+      chrome.storage.local.set(storage.items);
+    }
+  };
+
+  /**
+  * Add Open All button
+  */
+  var _addButton = function() {
     var menu;
     var table = $(selectorTable);
 
@@ -35,9 +77,21 @@ var Links = (function () {
         menu.html(buttonHtml + menu.html())
 
         // Add listener
-        document.getElementById(selectorButton).addEventListener('click', _openLinks);
+        document.getElementById(selectorButton).addEventListener('click', _buttonHandler);
       }
     }
+  }
+
+  /**
+  * Initialize links module
+  */
+  var init = function() {
+    _addButton();
+
+    chrome.storage.local.get(storage.key, function(items) {
+      storage._initSet(items);
+      storage.limitSet(300);
+    });
   }
 
   return {
